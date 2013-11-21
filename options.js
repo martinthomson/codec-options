55b7a4b5f9356affbafc0f7a27aad6620212698e
Martin Thomson <martin.thomson@gmail.com>
2013-11-21 15:12:51 -0800

    Pretty printing.

--- options.js
+++ options.js
@@ -1,14 +1,14 @@
 var codecs = ["VP8","H264", "Theora", "H261", "H263"];
-var levels = ["MUST", "SHOULD", "MUST (BUT WE KNOW YOU WON'T)"];
+var levels = ["MUST", "SHOULD"];
 var counter = 0;
 function id(x) {
 	return x;
     }
 function times(a,b) { return a*b; }
 
-for (var counter = 0; counter < Math.pow(levels.length + 1,codecs.length); ++counter) {
+for (var counter = 0; counter < Math.pow(3,codecs.length); ++counter) {
     var reqs = codecs.map(function(codec, i) {
-	var x = Math.floor((counter / Math.pow(levels.length + 1, i))) % (levels.length + 1);
+	var x = Math.floor((counter / Math.pow(3, i))) % 3;
 	if (x > 0) {
 	    return {level: levels[x - 1], codec: codec };
 	}
@@ -36,7 +36,7 @@ for (var counter = 0; counter < Math.pow(levels.length + 1,codecs.length); ++cou
 	    var x = Math.floor(subsetCounter / subsetCounts.slice(0, i).reduce(times, 1));
 	    x %= subsetCounts[i];
 	    
-	    return l + ' implement ' + ((x === 0) ? 'all' : (subsetCounts[i] - x).toString()) + ' of { ' + reqs[l].join(', ') + ' }';
+	    return l + ' implement ' + ((reqs[l].length > 1) ? (((x === 0) ? 'all' : (subsetCounts[i] - x).toString()) + ' of { ' + reqs[l].join(', ') + ' }') : reqs[l][0]);
 	}).filter(id).join(' and '));
     }
 }
